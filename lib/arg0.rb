b7267268d142bc27e96ba61bcbf81dd85c344315
abhishekkr <abhikumar163@gmail.com>
2013-03-27 00:21:05 +0530

    test added [AND] arg0 methods values, value and keyring added

--- lib/arg0.rb
+++ lib/arg0.rb
@@ -22,5 +22,31 @@ module Arg0
       values.delete nil
       values
     end
+
+    class << self; alias values value_for; end
+
+    def self.value(switch_name)
+      ARGV.each_with_index {|arg, index|
+        if [switch_name].flatten.include? arg
+          return ARGV[index + 1]
+        end
+      }
+      nil
+    end
+
+    def self.keyring
+      key_vals = {}
+      ARGV.each {|arg|
+        last_key = key_vals.keys.last
+        if arg.match(/^-.*/)
+          key_vals[arg] = true
+        elsif key_vals[last_key] == true
+          key_vals[last_key] = arg
+        else
+          key_vals[arg] = :data
+        end
+      }
+      key_vals
+    end
   end
 end
---
+++ test/test_arg0_console.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+require 'test/unit'
+require_relative '../lib/arg0.rb'
+
+class TestArg0Console < Test::Unit::TestCase
+  def setup;   end
+  def teardown; ARGV.clear; end
+
+  def test_switch_empty
+    assert_equal Arg0::Console.switch?('-h'), false
+  end
+
+  def test_switch_on
+    ARGV.push '-h'
+    assert_equal Arg0::Console.switch?('-h'), true
+    assert_equal Arg0::Console.switch?(['-h', '--help']), true
+    assert_equal Arg0::Console.switch?(['-info', '-h', '--help']), true
+  end
+
+  def test_switch_off
+    ARGV.push '--help'
+    assert_equal Arg0::Console.switch?('-h'), false
+    assert_equal Arg0::Console.switch?(['-h', '-help']), false
+  end
+
+  def test_value_for
+    assert_equal Arg0::Console.value_for('-src'), []
+    ARGV.push '--log', 'a0.log', '-src', File.dirname(__FILE__), '-v', '-l', 'a2.log'
+    src01 = Arg0::Console.value_for('-src')
+    src02 = Arg0::Console.value_for(['-s', '-src', '--source'])
+    log = Arg0::Console.value_for(['-l', '--log'])
+    assert_equal src01, src02
+    assert_not_equal src01, log
+    assert_equal log, ['a0.log', 'a2.log']
+  end
+
+  def test_values
+    ARGV.push '--log', 'a0.log', '-src', File.dirname(__FILE__), '-v', '-l', 'a2.log'
+    src01 = Arg0::Console.values('-src')
+    src02 = Arg0::Console.values(['-s', '-src', '--source'])
+    log = Arg0::Console.values(['-l', '--log'])
+    assert_equal src01, src02
+    assert_not_equal src01, log
+    assert_equal log, ['a0.log', 'a2.log']
+  end
+
+  def test_value
+    ARGV.push '--log', 'a0.log', '-src', File.dirname(__FILE__), '-v', '-l', 'a2.log'
+    log = Arg0::Console.value(['-l', '--log'])
+    assert_equal log, 'a0.log'
+  end
+
+  def test_keyring
+    assert_equal Arg0::Console.keyring, {}
+    ARGV.push '--log', 'a0.log', '-src', File.dirname(__FILE__), '-v', '-l', 'a2.log'
+    key_vals = Arg0::Console.keyring
+    assert_equal key_vals, {"--log"=>"a0.log", "-src"=>File.dirname(__FILE__), "-v"=>true, "-l"=>"a2.log"}
+  end
+end
